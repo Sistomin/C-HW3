e57f2d49159292ac8db7e0445cd255c1419a615a
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-10-29 15:28:13 +0700

    add Task21 HW3

---
+++ Task21HW/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double distance;
+    distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+
+    return distance;
+}
+
+Console.WriteLine($"Введите координату по оси Х первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите координату по оси Y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите координату по оси Z первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите координату по оси Х второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите координату по оси Y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите координату по оси Z второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Distance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Растояние между точками равно {Math.Round(distance, 2)}");
